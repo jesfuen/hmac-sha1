4403f3193f7fdae07ac6958fcadc7cf8420fba41
jesfuen <jesusdelafuente2002@gmail.com>
2026-03-09 18:21:31 +0100

    añadir funcion create_hash

--- hash-sha1.c
+++ hash-sha1.c
@@ -4,33 +4,22 @@
 #include <openssl/evp.h>
 #include <string.h>
 
+enum {
+    BUFSIZE = 4096
+};
+
 void
 usage() {
     errx(EXIT_FAILURE,"usage: hash-sha1 str");
 }
 
-/*
 
-unsigned char*
-create_hash() {
 
-}
-
-*/
-
-int
-main(int argc, char *argv[]) {
-
+void
+create_hash(unsigned int *md_len, unsigned char *hash , FILE *fd) {  
     EVP_MD_CTX *mdctx;
-    unsigned char hash[EVP_MAX_MD_SIZE];
-    unsigned int md_len;
-    char mess1[] = "Test Message\n";
-
-    if (argc < 2) {
-        usage();
-    }
-
-    // Leer de stdin o de un fichero y pasar el contenido a create_hash()
+    unsigned char buffer[BUFSIZE];
+    size_t br;
 
     mdctx = EVP_MD_CTX_new();
     if (mdctx == NULL) {
@@ -41,27 +30,41 @@ main(int argc, char *argv[]) {
         errx(EXIT_FAILURE,"Message digest initialization failed.\n");
     }
 
-    for (int i = 0; i < argc; i++) {
-        if (!EVP_DigestUpdate(mdctx, argv[i], strlen(mess1))) {
-            errx(EXIT_FAILURE,"Message digest update failed.\n");
-        }
+    // Leer del fichero e ir actualizando
+    while ((br = fread(buffer, 1, sizeof(buffer), fd)) > 0) {
+        if (!EVP_DigestUpdate(mdctx, buffer, br))
+            errx(EXIT_FAILURE, "Message digest update failed.\n");
     }
 
-    /*
-    // Cambiar seccion a un bucle for para ir haciendo update
-    if (!EVP_DigestUpdate(mdctx, mess1, strlen(mess1))) {
-        errx(EXIT_FAILURE,"Message digest update failed.\n");
-    }
-    */
-
-    if (!EVP_DigestFinal_ex(mdctx,hash, &md_len)) {
+    if (!EVP_DigestFinal_ex(mdctx,hash, md_len)) {
         errx(EXIT_FAILURE,"Message digest finalization failed.\n");
     }
 
 
     EVP_MD_CTX_free(mdctx);
 
-    // hash = create_hash()
+}
+
+
+
+int
+main(int argc, char *argv[]) {
+
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
+    FILE *fd;
+
+    if (argc != 2) {
+        usage();
+    }
+
+    fd = fopen(argv[1], "r");
+    if (fd == NULL) {
+        err(EXIT_FAILURE,"error: open failed!");
+    }
+
+    create_hash(&md_len,hash,fd);
+    
 
     for (int i = 0; i < md_len; i++) {
         printf("%02x",hash[i]);
